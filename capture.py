59264472df1cff7b5dbd3b4ca5451c54ee7aefd5
Awolize <alex.store@hotmail.com>
2022-11-21 17:47:53 +0100

    if frame

--- capture.py
+++ capture.py
@@ -16,9 +16,9 @@ def getTime() -> str:
 
 def getFrame() -> npt.NDArray:
     cap: cv2.VideoCapture = cv2.VideoCapture(url)
-    _, frame = cap.read()
+    ret, frame = cap.read()
     cap.release()
-    return frame
+    return ret, frame
 
 
 url = "http://192.168.1.165:8081"
@@ -26,20 +26,19 @@ img_path = img_path = "images/" + "000000.png"  # init name
 freq_seconds = 60
 
 while True:
-    frame = getFrame()
+    ret, frame = getFrame()
+    if ret == True:
+        cv2.fastNlMeansDenoisingColored(frame, frame, 10, 10, 7, 21)
 
-    print(frame)
-    cv2.fastNlMeansDenoisingColored(frame, frame, 10, 10, 7, 21)
+        currentImages = glob.glob("images/0*.png")
+        if len(currentImages) > 0:
+            currentImages.sort()
+            currentImages[-1] = currentImages[-1].replace("//", "\\")
+            count = int(currentImages[-1].replace("images/", "").replace(".png", ""))
+            count = count + 1
+            img_path = "images/" + str(count).zfill(6) + ".png"
 
-    currentImages = glob.glob("images/0*.png")
-    if len(currentImages) > 0:
-        currentImages.sort()
-        currentImages[-1] = currentImages[-1].replace("//", "\\")
-        count = int(currentImages[-1].replace("images/", "").replace(".png", ""))
-        count = count + 1
-        img_path = "images/" + str(count).zfill(6) + ".png"
-
-    print(getTime(), img_path)
-    cv2.imwrite(img_path, frame)
+        print(getTime(), img_path)
+        cv2.imwrite(img_path, frame)
 
     time.sleep(freq_seconds)
